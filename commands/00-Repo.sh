3116262a40885a800d5be465596b1ce126ca9aa3
YeffaDev <paoloceffa@hotmail.com>
2024-02-25 01:22:11 +0100

    a

--- commands/00-Repo.sh
+++ commands/00-Repo.sh
@@ -1,16 +1,16 @@
 #Github : OpenHack Microsoft
+#Use "byos/containers/deploy/deploy.sh" to deploy the lab
 https://github.com/microsoft/OpenHack
 https://github.com/microsoft/OpenHack/blob/main/byos/containers/deployment.md
-Use "byos/containers/deploy/deploy.sh" to deploy the lab
 
 #Github : Azure-Samples/openhack-containers (archived-read-only)
-Containers source code but with bugs
-git clone https://github.com/Azure-Samples/openhack-containers.git
+#Containers source code with bugs
+https://github.com/Azure-Samples/openhack-containers.git
 
 #Github : LouAtAvanade
-Use this source code fit bugs fixed (workshop-lab)
-git clone https://github.com/LouAtAvanade/containers_artifacts
+#Source code fix bugs fixed (workshop-lab)
+https://github.com/LouAtAvanade/containers_artifacts
 
 #Github : YeffaDev/openhack-mycontainer
-Use this repo to configure the containers environment challenge
-git clone https://github.com/YeffaDev/OpenHack-MyContainer
+#Use this repo to configure the containers environment challenge
+https://github.com/YeffaDev/OpenHack-MyContainer
--- commands/03-Resources.sh
+++ commands/03-Resources.sh
@@ -29,5 +29,27 @@ az container show -n $simulatorName -g $teamRG
 #Add yourpublic IP to SQL server Network
 #use Azure Query editor to query tables on mydrivingDB
 
+#Github : YeffaDev/openhack-mycontainer
+#Use this repo to download source code and configuration files
+git clone https://github.com/YeffaDev/OpenHack-MyContainer
 
+#Create new repo with your name "OpenHack-YourNameContainer"
+#Use this repo to create your own configuration files
+https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
+#Configure github credential
+sudo git config --system --unset credential.helper
+git config --global user.name "YeffaDev"
+git config --global user.email "paoloceffa@hotmail.com"
+#Create a new repo in the github portal console
+#Create local repo
+cd ~/Openhack
+mkdir OpenHack-YourNameContainer
+echo "# OpenHack-YourNameContainerLab" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+#Sync remote repo
+git remote add origin https://github.com/YeffaDev/OpenHack-YourContainer.git
+git push -u origin main
 
